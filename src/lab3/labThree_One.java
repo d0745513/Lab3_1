a239980cd622da7cff6fb7067cc6bd11bce50821
d0745513 <56075069+d0745513@users.noreply.github.com>
2020-03-17 11:17:31 +0800

    Update labThree_One.java
    add text"oh! I see you.".

--- src/lab3/labThree_One.java
+++ src/lab3/labThree_One.java
@@ -4,7 +4,7 @@ public class labThree_One {
 
   public static void main(String[] args) {
     // TODO Auto-generated method stub
-    System.out.print("Dance Monkey.");
+    System.out.print("Dance Monkey.oh! I see you.");
   }
 
 }
