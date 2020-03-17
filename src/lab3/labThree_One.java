ca53f1cfa6a52f665d34ffb065e5d94020c5f8c9
d0745513 <56075069+d0745513@users.noreply.github.com>
2020-03-17 11:27:19 +0800

    Update labThree_One.java
    System.out.print("Dance Monkey.ohh! I see you,see you,see you everytime");

--- src/lab3/labThree_One.java
+++ src/lab3/labThree_One.java
@@ -4,7 +4,7 @@ public class labThree_One {
 
   public static void main(String[] args) {
     // TODO Auto-generated method stub
-    System.out.print("Dance Monkey.oh! I see you.");
+    System.out.print("Dance Monkey.ohh! I see you,see you,see you everytime");
   }
 
 }
